5c99870ce6b8bbccbd34da59ef1d90ee9c79d873
jason beedle <jasonbeedle@macbookpro.mynet>
2025-03-14 09:34:30 +0000

    header-bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
+"use client"
 import Image from "next/image";
 import logo from "@@/app/components/assets/logo.png";
+import HeaderBar from "./components/headerBar";
 
 export default function Home() {
   return (
     <div>
-      <h1>Logistics UK</h1>
-      <Image
-        src={logo}
-        alt="Logistics UK"
-        width={500}
-        height={500}
-      />
+      <HeaderBar />
     </div>
   );
 }
